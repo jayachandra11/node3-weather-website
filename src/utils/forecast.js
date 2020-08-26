bbca0d7197336ca1358a60df9cea1b2dee5d2fb0
jayachandra11 <jayachandra.nuthalapati11@gmail.com>
2020-08-26 10:26:54 +0530

    adding humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,9 +16,10 @@ else if(body.error)
     callback('Unable to find location',undefined);
 }
 else
-    callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degress out. It feels like "+body.current.feelslike+" degress out.");
+{
+    callback(undefined,body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degress out. It feels like "+body.current.feelslike+" degress out. The humidity is "+body.current.humidity+"%");
 
-
+}
 })
 }
 
